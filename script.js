84082a381d2ad9dddb86c155dfdc8832fc5095a9
Karim Gomez <55936@VNSNY.ORG>
2021-06-23 17:27:31 -0400

    updated js file

---
+++ script.js
@@ -0,0 +1,15 @@
+setInterval(function () {
+
+    $("#current-date-and-time").text(moment().format('MMMM Do YYYY, h:mm:ss a'))
+}, 1000)
+
+$(".fa-save").click(function (event) {
+    let key = $(this).parent().siblings('th').text()
+    let value = $(this).parent().siblings('.middle-column').children('textarea').val()
+
+    localStorage.setItem(key, value)
+})
+
+for (let i = 9; i < 18; i++) {
+    
+}
